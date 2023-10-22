39a1e9115345b356ded3a24190bcae25ac6abcf8
Your Name <you@example.com>
2023-10-21 20:16:37 -0600

    cambios

--- adminwebpani/src/App.vue
+++ adminwebpani/src/App.vue
@@ -1,32 +1,16 @@
 <!--HTML Aqui se escribe el codigo HTML de nuestra aplicacion  -->
 <template>
-  <h1>Hola</h1>
-  <!-- <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Bienvenido al administrador Web pani"/> -->
+  <!-- <CounterComposition /> -->
+  <ExampleSelective />
 </template>
 <!-- DATA La etiqueta script Va a contener la logica de los datos un componente y el lenguaje es TypeScript-->
-<script lang="ts">
-// import { defineComponent } from 'vue';
-// import HelloWorld from './components/HelloWorld.vue';
-
-// export default defineComponent({
-//   name: 'App',
-//   components: {
-//     HelloWorld
-//   }
-// });
+<script lang="ts" setup>
+//  import CounterComposition from './components/CounterCompositionAPI.vue';
+import ExampleSelective from './components/ExampleSelective.vue';
 </script>
 <!-- CSS La etiqueta que afecta el estilo CSS del componente  -->
 <style>
 h1 {
   color: green;
 }
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-} */
 </style>
